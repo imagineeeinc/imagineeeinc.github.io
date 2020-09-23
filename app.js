cf6d53798d8d40bd19626dc3ea966c6839001da3
Imagineee <imagineeelego@gmail.com>
2020-09-23 19:53:50 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -6,11 +6,3 @@ document.getElementById("menu").onclick = function() {
         m.className = 'hmenu';
     }
 }
-document.getElementById("menu").onmouseover = function() {
-    var m = document.getElementById("menus")
-    if (m.className != 'smenu') {
-        m.className = 'smenu';
-    } else {
-        m.className = 'hmenu';
-    }
-}
---
+++ sw.js
@@ -0,0 +1,40 @@
+importScripts('cache-polyfill.js');
+
+
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('airhorner').then(function(cache) {
+     return cache.addAll([
+       '',
+       'index.html',
+       'wood.html',
+       'game.html',
+       'style.css',
+       'wood.css',
+       'game.css',
+       'app.js',
+       'manifest.json',
+       'icon/Github.png',
+       'icon/open.png',
+       'icon/so.png',
+       'icon/user.png',
+       'img/back.gif',
+       'img/head_wall.png',
+       'img/mt.jpg',
+       'Favicon/favicon.ico',
+       'Favicon/apple-touch-icon.png',
+       'Favicon/maskable_icon.png',
+       'Favicon/android-chrome-512x512.png',
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(event) {
+    console.log(event.request.url);
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+        );
+    });
