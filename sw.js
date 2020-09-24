73243305879721658e2b891440829bb15839502d
imagineeeinc <imagineeelego@gmail.com>
2020-09-24 08:19:23 +0100

    changed sw.js

--- sw.js
+++ sw.js
@@ -31,32 +31,6 @@ self.addEventListener('install', function(e) {
  );
 });
 
-caches.open('imagineee').then(function(cache) {
-     return cache.addAll([
-       '',
-       'index.html',
-       'wood.html',
-       'game.html',
-       'style.css',
-       'wood.css',
-       'game.css',
-       'app.js',
-       'manifest.json',
-       'icon/Github.png',
-       'icon/open.png',
-       'icon/so.png',
-       'icon/user.png',
-       'img/back.gif',
-       'img/head_wall.png',
-       'img/mt.jpg',
-       'Favicon/favicon.ico',
-       'Favicon/apple-touch-icon.png',
-       'Favicon/maskable_icon.png',
-       'Favicon/android-chrome-512x512.png',
-       'https://github-readme-stats.vercel.app/api/top-langs/?username=imagineeeinc&hide=ruby'
-     ]);
-   })
-
 self.addEventListener('fetch', function(event) {
     console.log(event.request.url);
     event.respondWith(
