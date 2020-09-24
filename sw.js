93cab75c3e4bb5910cf226d84f1aa0f1a61ad3ff
Imagineee <imagineeelego@gmail.com>
2020-09-24 08:12:54 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 importScripts('cache-polyfill.js');
 
 
-/*self.addEventListener('install', function(e) {
- e.waitUntil(*/
+self.addEventListener('install', function(e) {
+ e.waitUntil(
    caches.open('imagineee').then(function(cache) {
      return cache.addAll([
        '',
@@ -28,8 +28,34 @@ importScripts('cache-polyfill.js');
        'https://github-readme-stats.vercel.app/api/top-langs/?username=imagineeeinc&hide=ruby'
      ]);
    })
- /*);
-});*/
+ );
+});
+
+caches.open('imagineee').then(function(cache) {
+     return cache.addAll([
+       '',
+       'index.html',
+       'wood.html',
+       'game.html',
+       'style.css',
+       'wood.css',
+       'game.css',
+       'app.js',
+       'manifest.json',
+       'icon/Github.png',
+       'icon/open.png',
+       'icon/so.png',
+       'icon/user.png',
+       'img/back.gif',
+       'img/head_wall.png',
+       'img/mt.jpg',
+       'Favicon/favicon.ico',
+       'Favicon/apple-touch-icon.png',
+       'Favicon/maskable_icon.png',
+       'Favicon/android-chrome-512x512.png',
+       'https://github-readme-stats.vercel.app/api/top-langs/?username=imagineeeinc&hide=ruby'
+     ]);
+   })
 
 self.addEventListener('fetch', function(event) {
     console.log(event.request.url);
